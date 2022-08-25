49b2e3b334a9613e205814be3ffa7add20e55397
Habeebuddin Mir <habeebuddinmir@gmail.com>
2022-08-26 03:03:34 +0530

    215. Kth Largest Element in an Array

--- src/com/codewithhabeeb/KthLargest.java
+++ src/com/codewithhabeeb/KthLargest.java
@@ -1,5 +1,22 @@
 package com.codewithhabeeb;
 
-public class KthLargest {
+import java.util.PriorityQueue;
 
+public class KthLargest {
+//When you want to find the largest/smallest elements efficiently, always try to find them using Heaps
+    static int findKthLargest( int[] nums, int k){
+        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(); //minHeap root will have the kth largest element
+        for(int i: nums){
+            minHeap.add(i); //this solution is of the complexity O(n log n)
+            if(minHeap.size() > k){
+                minHeap.remove();
+            }
+        }
+        return minHeap.remove();
+    }
+
+    public static void main(String[] args){
+        int[] arr = { 5, 10, 15, 20 };
+        System.out.println(findKthLargest(arr, 2));
+    }
 }
